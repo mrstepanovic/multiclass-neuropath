05820c35c95e83e281fc98b80b729d5dc52359d9
Mike Stepanovic <mstepanovic@mgh.harvard.edu>
2016-05-24 17:03:44 -0400

    update classifier

--- mclassifier.py
+++ mclassifier.py
@@ -2,6 +2,15 @@
 # pulled from stackoverflow -- barebone skeleton and will prob need lots of work
 
 import numpy as np
+from raw_etl import get_example, build_training_set
+
+example = get_example('data/lh_thickness.csv')
+
+print(example)
+
+
+# TO BE INCORPORATED
+'''
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
@@ -26,3 +35,4 @@ classifier.fit(X_train, y_train)
 predicted = classifier.predict(X_test)
 for item, labels in zip(X_test, predicted):
     print '%s => %s' % (item, ', '.join(target_names[x] for x in labels))
+    '''
